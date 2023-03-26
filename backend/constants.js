3d15e832f964751d8b8a1ebd457837f798dcfd04
yatharthvyas <yatharth2000.vyas@gmail.com>
2023-03-26 00:01:08 -0700

    add constants

---
+++ backend/constants.js
@@ -0,0 +1,18 @@
+exports.CITIES = ["LAX", "JFK", "LHR", "NRT", "SYD"];
+
+exports.HOTEL_AMENITIES = [
+  "Free Wi-Fi",
+  "Swimming pool",
+  "Fitness center",
+  "Restaurant",
+  "Bar",
+  "Room service",
+];
+
+exports.CITIES_MAP = {
+  LAX: "Los Angeles, USA",
+  JFK: "New York City, USA",
+  LHR: "London, UK",
+  NRT: "Tokyo, Japan",
+  SYD: "Sydney, Australia",
+};
